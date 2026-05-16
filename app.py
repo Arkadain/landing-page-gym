6cabb4dcbe5c63cb13adf54d46cae2bd6b795b74
Arkadain <arkadain405@gmail.com>
2026-05-16 20:26:26 -0300

    agregado de rutas

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ def inicio():
     # Esto busca automáticamente el index.html en la carpeta templates
     return render_template('index.html')
 
+@app.route('/maquinas')
+def maquinas():
+    return render_template('maquinas.html')
+
+@app.route('/ejercicios')
+def ejercicios():
+    return render_template('ejercicios.html')
+
 if __name__ == '__main__':
     # El debug=True hace que la página se actualice sola cuando cambiás el HTML
     app.run(debug=True)
